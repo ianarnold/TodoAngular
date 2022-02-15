52947c9707743a8ed328c7ec5f1475d0536b01e7
unisanta <unisanta@santacecilia.br>
2022-02-15 20:13:35 -0300

    finalized

---
+++ src/app/item/item.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Item } from '../item';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  editable = false;
+
+  @Input() item: Item;
+  @Input() newItem: string;
+  @Output() remove = new EventEmitter<Item>();
+
+  saveItem(description) {
+    if (!description) return;
+    this.editable = false;  
+    this.item.description = description;
+  }
+
+
+}
